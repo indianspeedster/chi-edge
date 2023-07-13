4ded1468e2292d835e47e9d11564a1b129f31f76
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2023-07-13 11:04:01 -0400

    Update model.py

--- python_code/model.py
+++ python_code/model.py
@@ -2,26 +2,31 @@ import tflite_runtime.interpreter as tflite
 from PIL import Image
 import numpy as np
 
+with open("labels.txt", 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
 
-interpreter = tflite.Interpreter(model_path='model.tflite')
+interpreter = tflite.Interpreter(model_path='mobilenet_v2_1.0_224_quantized_1_default_1.tflite')
 interpreter.allocate_tensors()
-
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 image_path = 'image.png'  
-image = Image.open(image_path).resize((224, 224))
-image = image.convert('RGB')
-input_image = np.array(image, dtype=np.float32)
-input_image = (input_image - 127.5) / 127.5  
-interpreter.set_tensor(input_details[0]['index'], [input_image])
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+img = Image.open(image_path).resize((width, height))
+img = img.convert('RGB')
+input_data = np.array(img)
+input_data = np.expand_dims(img, axis=0)
 
+interpreter.set_tensor(input_details[0]['index'], input_data)
 
 interpreter.invoke()
 
-
 output_data = interpreter.get_tensor(output_details[0]['index'])
 
+sorted_result = sorted((e,i) for i,e in enumerate(output_data[0]))
+top_3 = sorted_result[-3:][::-1]
 
-print(np.argmax(output_data[0]))
+for j,i in top_3:
+  print('{:08.6f}: {}'.format(float(j / 255.0), labels[i].split(":")[1]))
